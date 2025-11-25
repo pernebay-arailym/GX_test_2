5a5c0fb1ba1a8cbb913f6a0d3a7e14653da8c601
pernebay-arailym <pernebayarailym@gmail.com>
2025-11-25 15:54:10 +0100

    add functionality to display column names in the dataframe

--- column_count.py
+++ column_count.py
@@ -4,6 +4,8 @@ import pandas as pd
 url="https://raw.githubusercontent.com/great-expectations/gx_tutorials/main/data/yellow_tripdata_sample_2019-01.csv"
 df = pd.read_csv(url)
 
+list = df.columns.tolist()
+print("Column names in the dataframe:", list)
 #count how many times the value 1 appears in the 'passanger_count' column
 count = (df['passenger_count']==1).sum()
 print("Number of occurances of '1' in 'passenger column':", count)
